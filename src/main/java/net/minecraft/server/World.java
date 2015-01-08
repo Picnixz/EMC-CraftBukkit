08423dc96f223d0b5e4f2eaabb06f3b147fcbb38
Aikar <aikar@aikar.co>
2015-01-07 22:45:25 -0500

    Fix Cancelling BlockPlaceEvent triggering physics

--- src/main/java/net/minecraft/server/World.java
+++ src/main/java/net/minecraft/server/World.java
@@ -602,6 +602,7 @@ public abstract class World implements IBlockAccess {
     }
 
     public void applyPhysics(int i, int j, int k, Block block) {
+        if (captureBlockStates) { return; } // CraftBukkit - Cancel all physics during placement
         this.e(i - 1, j, k, block);
         this.e(i + 1, j, k, block);
         this.e(i, j - 1, k, block);
